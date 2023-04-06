0cc4f8dbed79a31bf0d27bddb9f2391391158464
[TheLegend-25] <[jeddrek1@gmail.com]>
2023-04-06 16:47:26 +0200

    login screen works <3

--- src/views/Login.js
+++ src/views/Login.js
@@ -4,25 +4,17 @@ export function Login(){
     checkbox.innerHTML=`
     <div class="srodek">
     <label class="switch">
-    <input type="checkbox" onclick={change()}>
+    <input type="checkbox" id="hehe">
     <span class="slider round"></span>
     </label> 
     </div>
-    <script>
-    function change(){
-        var i=2
-     }
-    </script>
+    
     `;
+     const switc = checkbox.querySelector("#hehe")
+
      
-    // checkbox.addEventListener('change', ()=>{
-    //     var i =2
-    //     return i
-    // })
 
     const section = document.createElement('section');
-    
-    if (i === 1){
     
     section.innerHTML=`
     <div id="login">
@@ -45,7 +37,36 @@ export function Login(){
             </div>
         </form>
         </div>
-    `;}
+    `;
+
+   switc.addEventListener('change', ()=>{
+
+    if (i % 2 == 0){
+    
+    section.innerHTML=`
+    <div id="login">
+        <div class="blok">
+            <h2>Zaloguj się na swoje konto</h2>
+        
+        <form>  
+            <div id="okna">  
+                <div>
+                  <label for="username">e-mail:</label>
+                  <input type="email" name="username">
+                </div>
+                <div>
+                    <label for="password">Hasło: </label>
+                    <input type="password" name="password">
+                </div>
+            </div>
+            <div>
+                <input id="loguj" type="submit" value="Loguj">
+            </div>
+        </form>
+        </div>
+    `;
+        i = i+1
+        }
     else{
         section.innerHTML=`
         <div id="login">
@@ -68,8 +89,67 @@ export function Login(){
             </div>
         </form>
         </div>`;
+        i = i+1
         };
-    section.append(checkbox)
+        })
+
+
+//********************************** */
+switc.addEventListener('open', ()=>{
+
+    if (i % 2 == 0){
     
-    return section;
+    section.innerHTML=`
+    <div id="login">
+        <div class="blok">
+            <h2>Zaloguj się na swoje konto</h2>
+        
+        <form>  
+            <div id="okna">  
+                <div>
+                  <label for="username">e-mail:</label>
+                  <input type="email" name="username">
+                </div>
+                <div>
+                    <label for="password">Hasło: </label>
+                    <input type="password" name="password">
+                </div>
+            </div>
+            <div class="srodek">
+                <input id="loguj" type="submit" value="Loguj">
+            </div>
+        </form>
+        </div>
+    `;
+        i = i+1
+        }
+    else{
+        section.innerHTML=`
+        <div id="login">
+        <div class="blok">
+            <h2>Zarejestruj swoje konto</h2>
+        
+        <form>  
+            <div id="okna">  
+                <div>
+                  <label for="username">e-mail:</label>
+                  <input type="email" name="username">
+                </div>
+                <div>
+                    <label for="password">Hasło: </label>
+                    <input type="password" name="password">
+                </div>
+            </div>
+            <div>
+                <input id="loguj" type="submit" value="Loguj">
+            </div>
+        </form>
+        </div>`;
+        i = i+1
+        };
+        })
+//************************************ */
+    checkbox.append(section)
+    
+    return checkbox;
 }
