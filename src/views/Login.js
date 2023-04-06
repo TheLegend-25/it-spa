1bf463394a5b18cefddc78996f902955d9ca4692
[TheLegend-25] <[jeddrek1@gmail.com]>
2023-04-06 16:49:47 +0200

    deleted useless code

--- src/views/Login.js
+++ src/views/Login.js
@@ -93,62 +93,6 @@ export function Login(){
         };
         })
 
-
-//********************************** */
-switc.addEventListener('open', ()=>{
-
-    if (i % 2 == 0){
-    
-    section.innerHTML=`
-    <div id="login">
-        <div class="blok">
-            <h2>Zaloguj się na swoje konto</h2>
-        
-        <form>  
-            <div id="okna">  
-                <div>
-                  <label for="username">e-mail:</label>
-                  <input type="email" name="username">
-                </div>
-                <div>
-                    <label for="password">Hasło: </label>
-                    <input type="password" name="password">
-                </div>
-            </div>
-            <div class="srodek">
-                <input id="loguj" type="submit" value="Loguj">
-            </div>
-        </form>
-        </div>
-    `;
-        i = i+1
-        }
-    else{
-        section.innerHTML=`
-        <div id="login">
-        <div class="blok">
-            <h2>Zarejestruj swoje konto</h2>
-        
-        <form>  
-            <div id="okna">  
-                <div>
-                  <label for="username">e-mail:</label>
-                  <input type="email" name="username">
-                </div>
-                <div>
-                    <label for="password">Hasło: </label>
-                    <input type="password" name="password">
-                </div>
-            </div>
-            <div>
-                <input id="loguj" type="submit" value="Loguj">
-            </div>
-        </form>
-        </div>`;
-        i = i+1
-        };
-        })
-//************************************ */
     checkbox.append(section)
     
     return checkbox;
